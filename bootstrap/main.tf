9df7169885d044040bd23871588129f929561ff9
abtnzg <bobaker.tn@gmail.com>
2026-05-31 12:52:41 +0000

    add rundom

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,9 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.state_bucket_name
+ bucket = "${var.state_bucket_name}-${random_id.bucket_suffix.hex}"
 
   tags = merge(var.tags, {
     Name = var.state_bucket_name
