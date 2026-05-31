57fc3dd70af926de9016a9c9900db9eb631b0065
abtnzg <bobaker.tn@gmail.com>
2026-05-31 12:59:52 +0000

    rudom

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -38,7 +38,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.locks_table_name
+  name         = "${var.locks_table_name}-${random_id.bucket_suffix.hex}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
