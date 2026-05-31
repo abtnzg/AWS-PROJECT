14835a502d3569e04333ac72b5d4582095563ce4
abtnzg <bobaker.tn@gmail.com>
2026-05-31 14:07:18 +0000

    update tree

--- Terraform-infrastr/backend.tf
+++ Terraform-infrastr/backend.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "s3" {
-    bucket         = "aws-project-terraform-state"
-    key            = "Terraform-infrastr/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
